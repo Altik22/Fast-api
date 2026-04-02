b0636d0afad2bdccea829b3e45a5baa9a01b8ddb
Altik22 <erbosynaltair@gmail.com>
2026-04-02 16:39:46 +0500

    updated cors

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"], 
+    allow_origins=["*"], # Allow everything for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
